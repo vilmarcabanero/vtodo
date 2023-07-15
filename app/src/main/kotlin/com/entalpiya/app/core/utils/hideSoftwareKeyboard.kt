5060cbf73df2483606a19a2c69cc3109e67a13e7
Vilmar Cabañero <vilmarcabanero@gmail.com>
2023-07-15 17:17:21 +0800

    F11: Done implementing feature 11, trigger create todo when return key on keyboard is clicked.

---
+++ app/src/main/kotlin/com/entalpiya/app/core/utils/hideSoftwareKeyboard.kt
@@ -0,0 +1,10 @@
+package com.entalpiya.app.core.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun hideSoftwareKeyboard(view: View) {
+    val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    imm?.hideSoftInputFromWindow(view.windowToken, 0)
+}
--- app/src/main/kotlin/com/entalpiya/app/index/presentation/add_edit_todo/AddEditTodoScreen.kt
+++ app/src/main/kotlin/com/entalpiya/app/index/presentation/add_edit_todo/AddEditTodoScreen.kt
@@ -1,19 +1,19 @@
 package com.entalpiya.app.index.presentation.add_edit_todo
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -24,20 +24,20 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.entalpiya.app.core.presentation.components.FloatingAction
+import com.entalpiya.app.core.utils.hideSoftwareKeyboard
 import com.entalpiya.app.index.domain.model.Todo
 import com.entalpiya.app.index.presentation.destinations.TodoListScreenDestination
 import com.entalpiya.app.index.presentation.todo_list.TodoListViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import kotlinx.coroutines.launch
 import java.util.UUID
 
 @Destination
@@ -50,6 +50,8 @@ fun AddEditTodoScreen(
 ) {
     val scaffoldState = rememberScaffoldState()
     val focusRequester = remember { FocusRequester() }
+    val view = LocalView.current
+
     LaunchedEffect(Unit) {
         if (todo == null) {
             focusRequester.requestFocus()
@@ -58,6 +60,7 @@ fun AddEditTodoScreen(
             vm.setDescription(todo.description ?: "")
         }
     }
+
     Scaffold(
         topBar = {
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
@@ -88,22 +91,7 @@ fun AddEditTodoScreen(
         floatingActionButton = {
             FloatingAction(
                 onClick = {
-                    if (vm.state.value.addTodoSuccess) {
-                        vm.insertTodo(
-                            Todo(
-                                id = todo?.id ?: UUID.randomUUID().toString(),
-                                title = vm.titleState.value,
-                                description = vm.descriptionState.value,
-                                isComplete = todo?.isComplete ?: false,
-                                createdAt = todo?.createdAt ?: System.currentTimeMillis(),
-                            )
-                        )
-                        navigator.navigate(
-                            TodoListScreenDestination(
-                                todo = null
-                            )
-                        )
-                    }
+                    handleAddEditTodo(vm, todo, navigator)
                 },
                 icon = Icons.Default.Check,
                 desc = "check"
@@ -127,8 +115,13 @@ fun AddEditTodoScreen(
                     .fillMaxWidth()
                     .padding(10.dp)
                     .focusRequester(focusRequester),
-
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    hideSoftwareKeyboard(view)
+                    handleAddEditTodo(vm, todo, navigator)
+                })
                 )
+
             TextField(
                 value = vm.descriptionState.value,
                 onValueChange = { desc -> vm.setDescription(desc) },
@@ -138,8 +131,32 @@ fun AddEditTodoScreen(
                 ),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(10.dp)
+                    .padding(10.dp),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    hideSoftwareKeyboard(view)
+                    handleAddEditTodo(vm, todo, navigator)
+                })
             )
         }
+    }
+}
+
+fun handleAddEditTodo(vm: AddEditTodoViewModel, todo: Todo?, navigator: DestinationsNavigator) {
+    if (vm.state.value.addTodoSuccess) {
+        vm.insertTodo(
+            Todo(
+                id = todo?.id ?: UUID.randomUUID().toString(),
+                title = vm.titleState.value,
+                description = vm.descriptionState.value,
+                isComplete = todo?.isComplete ?: false,
+                createdAt = todo?.createdAt ?: System.currentTimeMillis(),
+            )
+        )
+        navigator.navigate(
+            TodoListScreenDestination(
+                todo = null
+            )
+        )
     }
 }
